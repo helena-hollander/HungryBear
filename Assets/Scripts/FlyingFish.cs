ee2b8b7c4afb35e2f4e456a512e7e2949784c08b
Helena Hollænder <helena.hollaender@gmail.com>
2025-05-08 11:33:02 +0200

    flying_fish_start

---
+++ Assets/Scripts/FlyingFish.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FlyingFish : MonoBehaviour
+{
+    public Vector3 hookPos;
+    public FishScript fishPos;
+
+   
+    void Update()
+    {
+        if(//fish close to hookPos ){
+           //navMesh pos target er hookPos
+           //start noise wiggle
+           //}
+    }
+}
